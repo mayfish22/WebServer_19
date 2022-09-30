14675a625a3754e3659ee9d62af702097be9a9fd
mayfish <mayfish22@gmail.com>
2022-10-01 01:17:35 +0800

    加入專案檔案。

---
+++ WebServer/Models/SigninViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebServer.Models
+{
+    public class SigninViewModel
+    {
+        [Display(Name = "Account")]
+        [Required(ErrorMessage = "AccountRequired")]
+        public string? Account { get; set; }
+        [Display(Name = "密碼")]
+        [Required(ErrorMessage = "密碼必填")]
+        public string? Password { get; set; }
+        public string? ReturnUrl { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
